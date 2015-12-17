1f4e2c7dea5c95b2398f2885665c083f9ad2add3
psxmz9508 <psxmz9508@icloud.com>
2015-12-17 14:51:49 +0800

    Initial Commit

---
+++ OSHomeWork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OSHomeWork
+//
+//  Created by 肖梦臻 on 15/12/17.
+//  Copyright © 2015年 肖梦臻. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
